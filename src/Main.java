4da05c39b5de4bf0372bb53eeab944c9403585ab
MustafaYacoub <mustafa.yacoub2016@yandex.ru>
2024-04-03 14:40:39 +0300

    First Commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,4 @@
 import java.util.Scanner;
-
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
@@ -16,7 +15,7 @@ public class Main {
     public static String calc(String input){
         if (!input.matches("\\d+ [+-/*] \\d+")) {
 
-            throw new IllegalArgumentException("т.к. формат математической операции не удовлетворяет заданию - два операнда и один оператор (+, -, /, *)\n");
+            throw new IllegalArgumentException("throws Exception // т.к. формат математической операции не удовлетворяет заданию - два операнда и один оператор (+, -, /, *)\n");
         }
         String[] parts = input.split(" ");
         if (parts.length == 1 || parts.length == 2) {
@@ -27,27 +26,17 @@ public class Main {
 
 
         if ( num1 < 1 || num1 > 10 || num2 < 1 || num2 > 10) {
-            throw new IllegalArgumentException("Допустимы только целые числа от 1 до 10\n");
+            throw new IllegalArgumentException("throws Exception // т.. допустимы только целые числа от 1 до 10\n");
         }
 
-        int result = 0;
-
-        switch (parts[1]) {
-            case "+":
-                result = num1 + num2;
-                break;
-            case "-":
-                result = num1 - num2;
-                break;
-            case "*":
-                result = num1 * num2;
-                break;
-            case "/":
-                result = num1 / num2;
-                break;
-            default:
-                throw new IllegalArgumentException("throws Exception //т.к. строка не является математической операцией \n");
-        }
+        int result = switch (parts[1]) {
+            case "+" -> num1 + num2;
+            case "-" -> num1 - num2;
+            case "*" -> num1 * num2;
+            case "/" -> num1 / num2;
+            default ->
+                    throw new IllegalArgumentException("throws Exception //т.к. строка не является математической операцией \n");
+        };
 
         return String.valueOf(result);
 
